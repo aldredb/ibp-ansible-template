a7384291c6e2331ad1bb1971afe7acec3025f0d4
Aldred Benedict <aldred@sg.ibm.com>
2020-10-26 10:36:51 +0800

    Added check so that channel creator is part of consortium

--- scripts/network.sh
+++ scripts/network.sh
@@ -34,12 +34,49 @@ function check_dep() {
 			exit 1
 		fi
 	done
-
 }
 
 function success_exit() {
 	echo "done"
 	exit 0
+}
+
+function set_org_vars() {
+  PEER_ORG_ARRAY=($(echo $(yq r vars/organizations.yaml "peer_organizations" | yq r - --printMode p "*") | tr "\n" "\n"))
+  ORDERER_ORG_ARRAY=($(echo $(yq r vars/organizations.yaml "ordering_organization" | yq r - --printMode p "*") | tr "\n" "\n"))
+  CHANNEL_ARRAY=($(echo $(yq r vars/channels.yaml "channels" | yq r - --printMode p "*") | tr "\n" "\n"))
+  CONSORTIUM_MEMBERS_ARRAY=($(echo $(yq r vars/channels.yaml "consortium_members.*") | tr "\n" "\n"))
+
+  if [ ${#CONSORTIUM_MEMBERS_ARRAY} -lt 1 ]; then
+    echo "There are no consortium members"
+    exit 1
+  fi
+
+  # Assume the first orderer organization
+  if [ ${#ORDERER_ORG_ARRAY} -gt 0 ]; then
+    ORDERER_ORG=${ORDERER_ORG_ARRAY[0]}
+  else
+    echo "There are no ordering organizations"
+    exit 1
+  fi
+
+	for ch in "${CHANNEL_ARRAY[@]}"; do
+    CHANNEL_MEMBERS_ARRAY=($(echo $(yq r vars/channels.yaml "channels[$ch].members" | yq r - --printMode p "*") | tr "\n" "\n"))
+    INTERSECT=()
+    for channel_member in "${CHANNEL_MEMBERS_ARRAY[@]}"; do
+        for consortium_member in "${CONSORTIUM_MEMBERS_ARRAY[@]}"; do
+            if [[ $channel_member = $consortium_member ]]; then
+                INTERSECT+=("$channel_member")
+            fi
+        done
+    done
+    if [ ${#INTERSECT} -lt 1 ]; then
+      echo "There are no channel members for $ch that belong to the consortium"
+      exit 1
+    fi
+    CREATOR_ORG=${INTERSECT[0]}
+    echo "Creator Org for $ch - $CREATOR_ORG"
+  done
 }
 
 while getopts ":hb:" opt; do
@@ -85,48 +122,33 @@ check_dep
 echo "done"
 
 printf "%s\n" "🔍 Listing organizations, channels and consortium members ... "
-PEER_ORG_LIST=$(yq r vars/organizations.yaml "peer_organizations" | yq r - --printMode p "*")
-ORDERER_ORG_LIST=$(yq r vars/organizations.yaml "ordering_organization" | yq r - --printMode p "*")
-ORDERER_ORG_ARRAY=($(echo $ORDERER_ORG_LIST | tr "\n" "\n"))
-CHANNEL_LIST=$(yq r vars/channels.yaml "channels" | yq r - --printMode p "*")
-CONSORTIUM_MEMBERS_LENGTH=$(yq r vars/channels.yaml --length "consortium_members")
+set_org_vars
 
-if [ $CONSORTIUM_MEMBERS_LENGTH -gt 0 ]; then
-	# Assume the first organization
-  FIRST_CONSORTIUM_MEMBER=$(yq r vars/channels.yaml "consortium_members[0]")
-else
-  echo "There are no consortium members"
-  exit 1
-fi
-
-if [ ${#ORDERER_ORG_ARRAY} -gt 0 ]; then
-	# There should only be 1 ordering organization
-  ORDERER_ORG=${ORDERER_ORG_ARRAY[0]}
-else
-  echo "There are no ordering organizations"
-  exit 1
-fi
+echo ""
+echo "Consortium Members:"
+for member in "${CONSORTIUM_MEMBERS_ARRAY[@]}"; do
+    echo "$member"
+done
 
 echo "" 
 echo "Peer Organizations:"
-for i in $PEER_ORG_LIST; do
+for i in "${PEER_ORG_ARRAY[@]}"; do
   echo $i
 done
 
 echo "" 
 echo "Ordering Organizations:"
-for i in $ORDERER_ORG_LIST; do
+for i in "${ORDERER_ORG_ARRAY[@]}"; do
   echo $i
 done
 
 echo ""
 echo "Channels:"
-for i in $CHANNEL_LIST; do
+for i in "${CHANNEL_ARRAY[@]}"; do
   echo $i
 done
 
 echo ""
-echo "Consortium's first member: $FIRST_CONSORTIUM_MEMBER"
 echo "Orderer Organization: $ORDERER_ORG"
 echo "done"
 
@@ -136,11 +158,11 @@ up)
 	echo "🚀 Setting up your IBP Platform ... "
 	ansible-playbook 00-create-folders.yaml
 
-	for org in $ORDERER_ORG_LIST; do
+	for org in "${ORDERER_ORG_ARRAY[@]}"; do
   	ansible-playbook 01-create-ordering-org.yaml --extra-vars "org_name=$org" -v
 	done
 
-	for org in $PEER_ORG_LIST; do
+	for org in "${PEER_ORG_ARRAY[@]}"; do
   	ansible-playbook 02-create-peer-orgs.yaml --extra-vars "org_name=$org" -v
 	done
 
@@ -152,23 +174,42 @@ up)
 
 	ansible-playbook 04-add-orgs-to-consortium.yaml --extra-vars "os_org_name=$ORDERER_ORG" -v
 
-	for ch in $CHANNEL_LIST; do
-  	./scripts/generate_channel_policies.sh $ch
+	for ch in "${CHANNEL_ARRAY[@]}"; do
+  	CHANNEL_MEMBERS_ARRAY=($(echo $(yq r vars/channels.yaml "channels[$ch].members" | yq r - --printMode p "*") | tr "\n" "\n"))
+  
+		INTERSECTION=()
+
+		for channel_member in "${CHANNEL_MEMBERS_ARRAY[@]}"; do
+				for consortium_member in "${CONSORTIUM_MEMBERS_ARRAY[@]}"; do
+						if [[ $channel_member = $consortium_member ]]; then
+								INTERSECTION+=("$channel_member")
+						fi
+				done
+		done
+
+  	CREATOR_ORG=${INTERSECTION[0]}
+		echo "Creator Org for $ch - $CREATOR_ORG"
+
+		./scripts/generate_channel_policies.sh $ch
+
 		ansible-playbook 05-create-channel.yaml \
-			--extra-vars "channel_name=$ch os_org_name=$ORDERER_ORG creator_org_name=$FIRST_CONSORTIUM_MEMBER" -v
+			--extra-vars "channel_name=$ch os_org_name=$ORDERER_ORG creator_org_name=$CREATOR_ORG" -v
 	done
 
 	if "$OPT_BEFORE_JOIN"; then
 		success_exit
 	fi
 
-	for ch in $CHANNEL_LIST; do
+	for ch in "${CHANNEL_ARRAY[@]}"; do
     # Retrieve channel members for the channel
     CHANNEL_MEMBERS=$(yq r vars/channels.yaml "channels[$ch].members" | yq r - --printMode p "*")
   	for channel_member in $CHANNEL_MEMBERS; do
+
+			echo "Joining peers from organization: $channel_member to channel: $ch"
       ansible-playbook 06-join-peers-to-channel.yaml \
 				--extra-vars "channel_name=$ch os_org_name=$ORDERER_ORG peer_org_name=$channel_member" -v
 
+			echo "Adding anchor peers from organization: $channel_member to channel: $ch"
 			ansible-playbook 07-add-anchor-peer-to-channel.yaml \
 				--extra-vars "channel_name=$ch os_org_name=$ORDERER_ORG peer_org_name=$channel_member" -v
     done
@@ -180,11 +221,11 @@ down)
 	echo "🧹 Tearing down ... "
 	ansible-playbook 97-remove-imported-components.yaml
 
-	for org in $PEER_ORG_LIST; do
+	for org in "${PEER_ORG_ARRAY[@]}"; do
   	ansible-playbook 98-delete-peer-orgs.yaml --extra-vars "org_name=$org" -v
 	done
 
-	for org in $ORDERER_ORG_LIST; do
+	for org in "${ORDERER_ORG_ARRAY[@]}"; do
   	ansible-playbook 99-delete-ordering-org.yaml --extra-vars "org_name=$org" -v
 	done
 
